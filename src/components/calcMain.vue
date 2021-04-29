3e6fd47d815a2ee4571d0f7a417fe2e76e8de5d4
edakhmetov <eduardakhmetov@pm.me>
2021-04-28 21:07:42 -0400

    initial changes

---
+++ src/components/calcMain.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="main">
+        <div class="display">
+
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Calculator',
+    props: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
