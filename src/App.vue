3b3ce2d7bfb1d207b5cd7defd4737c1685661029
edakhmetov <eduardakhmetov@pm.me>
2021-04-29 17:56:09 -0400

    added buttons and initial styling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  
+  <Calculator />
 </template>
 
 <script>
+    import Calculator from './components/calcMain'
 
+    export default {
+        components: {
+            Calculator
+        }
+    }
 </script>
 
 <style>
-
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        padding: 0;
+        margin: 0;
+    }
 </style>
--- src/components/calcMain.vue
+++ src/components/calcMain.vue
@@ -1,9 +1,27 @@
 <template>
     <div class="main">
-        <div class="display">
-
+        <div class="output">
+            <div class="previousOperand"></div>
+            <div class="currentOperand"></div>
         </div>
-        
+        <button style="span-two">AC</button>
+        <button>Del</button>
+        <button>÷</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>+</button>
+        <button>.</button>
+        <button>0</button>
+        <button style="span-two">=</button>
     </div>
 </template>
 
@@ -17,5 +35,9 @@ export default {
 </script>
 
 <style scoped>
-
+    .main {
+        display: flex;
+        flex-direction: column;
+        
+    }
 </style>
