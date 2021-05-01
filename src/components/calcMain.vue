f14a02a11359063010ccf80bcb4710051f978b7a
edakhmetov <eduardakhmetov@pm.me>
2021-04-30 20:13:39 -0400

    added logic to numbers and operations

--- src/components/calcMain.vue
+++ src/components/calcMain.vue
@@ -1,26 +1,26 @@
 <template>
     <div class="main">
         <div class="output">
-            <div class="previousOperand">{{operand1}}</div>
-            <div class="currentOperand"></div>
+            <div class="previousOperand">{{previousOperand}}</div>
+            <div class="currentOperand">{{currentOperand}}</div>
         </div>
         <button class="span-two">AC</button>
         <button>Del</button>
-        <button>÷</button>
-        <button @click="addNumber()">1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>*</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>+</button>
-        <button>.</button>
-        <button>0</button>
+        <button class="operation">÷</button>
+        <button @click="enterNumber(1)">1</button>
+        <button @click="enterNumber(2)">2</button>
+        <button @click="enterNumber(3)">3</button>
+        <button class="operation">*</button>
+        <button @click="enterNumber(4)">4</button>
+        <button @click="enterNumber(5)">5</button>
+        <button @click="enterNumber(6)">6</button>
+        <button class="operation">-</button>
+        <button @click="enterNumber(7)">7</button>
+        <button @click="enterNumber(8)">8</button>
+        <button @click="enterNumber(9)">9</button>
+        <button class="operation">+</button>
+        <button @click="addDecimal()">.</button>
+        <button @click="enterNumber(0)">0</button>
         <button class="span-two">=</button>
     </div>
 </template>
@@ -31,15 +31,33 @@ export default {
     props: {
 
     },
+    mounted: function() {
+        const operationButtons = document.querySelectorAll('.operation');
+        operationButtons.forEach((button) => {
+            button.addEventListener('click', () => {
+                this.chooseOperation(button.innerText);
+            })
+        })
+    },
     data() {
         return {
-            operand1: '',
-            operand2: '',
+            previousOperand: '',
+            currentOperand: '',
+            operation: undefined
         }
     },
     methods: {
-        addNumber () {
-            this.operand1 += `1`;
+        enterNumber (num) {
+            this.currentOperand += `${num}`;
+        },
+        addDecimal () {
+            if (this.currentOperand.includes('.')) return
+            this.currentOperand += '.';
+        },
+        chooseOperation (operation) {
+            this.operation = operation;
+            this.previousOperand = this.currentOperand;
+            this.currentOperand = '';
         }
     }
 }
